851f182c2956a389a437c5018e9413f92ee81ad1
flash <nexus2493@gmail.com>
2019-07-18 15:35:56 +0900

    first commit

---
+++ mainsite/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Board(models.Model):
+    title = models.CharField(max_length=30)
+    userName = models.CharField(max_length=30)
+    contents = models.TextField()
+    lookup = models.IntegerField(default=0)
---
+++ mainsite/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new/', views.newBoard, name='newBoard'),
+    path('list/', views.listBoard, name='listBoard'),
+    path('view/<int:id>/', views.viewBoard, name='viewBoard'),
+    path('updateDelete/<int:id>', views.updateDelete, name='updateDelete'),
+    path('updatepage/<int:id>',views.updatepage, name='updatepage'),
+]
